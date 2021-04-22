2170e91825b668ca007ad3f81671dacd3039801d
guanghes <guanghes@gmail.com>
2021-04-21 23:49:42 -0700

    Update build_app_image.sh

--- build_app_image.sh
+++ build_app_image.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-BASE_DIR=/var/lib/jenkins/release/production/
+BASE_DIR=/var/lib/jenkins/workspace/th3server/
 cd $BASE_DIR
 current_date=`TZ=America/Los_Angeles date +%Y-%m%d%H%M`
-docker build -t th3-server:$current_date -f /var/lib/jenkins/release/production/Dockerfile .
+docker build -t guanghes/th3-server:$current_date -f /var/lib/jenkins/workspace/th3server/Dockerfile .
 docker push guanghes/th3-server:$current_date
