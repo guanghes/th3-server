87bed56355e0e08d0310ff92aa0cc29eab122a3b
guanghes <guanghes@gmail.com>
2021-04-24 12:07:40 -0700

    Update th3-server.py

--- th3-server.py
+++ th3-server.py
@@ -8,7 +8,7 @@ from typing import Any, Dict, Callable, Union, List
 import redis
 from bottle import request, response, Bottle
 
-__version__ = "0.0.10"
+__version__ = "0.0.11"
 
 TRANSLATIONS = {
     "Victory or Death": "Lok'Tar Ogar",
